f8c4a1834d2cb24d129ec1778b6223dec92f15d4
ZoSand <64746594+ZoSand@users.noreply.github.com>
2022-11-21 17:11:38 +0100

    Correct output and hidden DLLs added to build folder (just in case)

--- WallPepper/main.cpp
+++ WallPepper/main.cpp
@@ -1,8 +1,12 @@
-#include <iostream>
-
 #include "../WindowsImpl/Wallpaper.h"
 
 int main() {
     Pepper::Impl::Wallpaper wp;
+    for (;;)
+    {
+        Win32::Sleep(10000);
+        break;
+    }
+
     return 0;
 }
